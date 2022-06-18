a08c76d92c77986244d5c6811cb917e7be29913c
Pakki Venkata Phani Chaitanya <venkataphanichaitanya@gmail.com>
2022-06-18 17:37:12 +0530

    Update streamlit_app.py
    Added header for smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
