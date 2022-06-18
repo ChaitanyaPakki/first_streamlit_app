d63be3f93a3a19d28a4eb01c0665f47077c1d9d6
Pakki Venkata Phani Chaitanya <venkataphanichaitanya@gmail.com>
2022-06-18 17:30:30 +0530

    Update streamlit_app.py
    Gave space after tittle attribute

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Diner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
