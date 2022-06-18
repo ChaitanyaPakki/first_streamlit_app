0bc4bcde9b2ac38f17c976bb23e0d4a371da2d3d
Pakki Venkata Phani Chaitanya <venkataphanichaitanya@gmail.com>
2022-06-18 17:34:48 +0530

    Update streamlit_app.py
    Added images to the menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My parents new Healthy Diner')
+streamlit.title("My Mom's new Healthy Diner")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
