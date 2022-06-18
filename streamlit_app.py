b889e85410da75fc979a82b4fa08bc2c01f9b5cd
Pakki Venkata Phani Chaitanya <venkataphanichaitanya@gmail.com>
2022-06-18 17:54:38 +0530

    Update streamlit_app.py
    Setting Fruit name as index on multiselect attribute

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # to read the CSV file from S3 bucket
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
+# setting Fruit name as index
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Multiselect option on fruits
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # display the file content on the streamlit app as a table
