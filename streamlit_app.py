9e593fefefdfa8b031514aa08fe53b1b62c2df73
Pakki Venkata Phani Chaitanya <venkataphanichaitanya@gmail.com>
2022-06-20 21:30:39 +0530

    Update streamlit_app.py
    Added json() function call to display the API response

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the file content on the streamlit app as a table
 streamlit.dataframe(fruits_to_show)
 
+#Section to display API response
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
