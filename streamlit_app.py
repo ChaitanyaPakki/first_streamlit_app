6d7752948b50b4e8e617193ccd0002f840335f0d
Pakki Venkata Phani Chaitanya <venkataphanichaitanya@gmail.com>
2022-06-20 21:42:44 +0530

    Update streamlit_app.py
    removing raw JSON and separating base URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ streamlit.dataframe(fruits_to_show)
 #Section to display API response
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json())
 
 #normalising JSON version of data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
