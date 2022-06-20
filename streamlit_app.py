ddca61c96787eebea425ecfc3a99d1630c2068fd
Pakki Venkata Phani Chaitanya <venkataphanichaitanya@gmail.com>
2022-06-20 21:47:59 +0530

    Update streamlit_app.py
    To display the API response dynamically by taking user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,10 @@ streamlit.dataframe(fruits_to_show)
 
 #Section to display API response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit you would love to know about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 #normalising JSON version of data
