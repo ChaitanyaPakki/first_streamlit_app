276df0c5e1e72d44d24b9015347cd34f57d9968f
Pakki Venkata Phani Chaitanya <venkataphanichaitanya@gmail.com>
2022-06-22 16:41:47 +0530

    Update streamlit_app.py
    Added exception block to the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,19 +30,19 @@ streamlit.dataframe(fruits_to_show)
 
 #Section to display API response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit you would love to know about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-#normalising JSON version of data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-#output the response as table
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop()
+try:
+  fruit_choice = streamlit.text_input('What fruit you would love to know about?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select to get fruit information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #normalising JSON version of data
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
+  streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
